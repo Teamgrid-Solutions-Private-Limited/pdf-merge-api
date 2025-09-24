a985414a736b5842ef1bd90f32314d6b5e332de0
SKSARUK10 <saruksoft2000@gmail.com>
2025-09-24 16:10:26 +0530

    add global check to allow only POST requests

--- routes/pdfRoutes.js
+++ routes/pdfRoutes.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const { processRecordsHandler } = require("../controllers/pdfController");
 const protectedKey = require("../utils/protectedKey");
-
 const router = express.Router();
+
+
+router.all(["/DemoMYGA", "/DemoFIA"], (req, res, next) => {
+  if (req.method !== "POST") {
+    return res
+      .status(405)
+      .json({ message: "Method not allowed. Please use POST." });
+  }
+  next();
+});
 
 router.post(
   "/DemoMYGA",
